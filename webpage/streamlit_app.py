42d4a2f7701219a5ad0f13531528f9bbafc37e80
artpj8192 <113569157+artpj8192@users.noreply.github.com>
2022-09-24 13:20:27 +0700

    Update streamlit_app.py

--- webpage/streamlit_app.py
+++ webpage/streamlit_app.py
@@ -1,7 +1,7 @@
 from PIL import Image
 import requests
 import streamlit as st
-from streamlit_lottie as st_lottie
+from streamlit_lottie import st_lottie
 st.set_page_config(page_title="My Webpage", page_icon=":smile:", layout="wide")
 
 def load_lottieurl(url):
